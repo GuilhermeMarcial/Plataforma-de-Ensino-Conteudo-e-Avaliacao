341082e2d9923d75e0db784f29b0ba08f60a4f25
G. Marcial <guilherme.marcial.felipe@gmail.com>
2017-04-05 16:38:51 -0400

    algumas refaforações

--- src/PlataformaDeEnsino.Presenter/Startup.cs
+++ src/PlataformaDeEnsino.Presenter/Startup.cs
@@ -8,23 +8,20 @@ namespace PlataformaDeEnsino.Presenter
 {
     public class Startup
     {
-        public Startup(IConfigurationRoot configuration)
-        {
-            this.Configuration = configuration;
-
-        }
         public IConfigurationRoot Configuration { get; }
         public Startup(IHostingEnvironment env)
         {
-
+            
         }
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddMvc();
         }
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole();
+            app.UseDeveloperExceptionPage();
+            app.UseMvc();
         }
     }
 }
