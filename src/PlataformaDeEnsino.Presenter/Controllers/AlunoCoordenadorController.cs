fca8574de7e93c7c19c05934bf4240438862da6c
Guilherme Marcial <guilherme.marcial.felipe@gmail.com>
2017-05-22 15:04:35 -0400

    Logica de download encapsulada, reorganizado os namespace pelo seu contexto, organizado o registro de dependencias pelo seu contexto, diminuindo o inchaço na "ConfigureServices" da startup.cs,

--- src/PlataformaDeEnsino.Presenter/Controllers/AlunoCoordenadorController.cs
+++ src/PlataformaDeEnsino.Presenter/Controllers/AlunoCoordenadorController.cs
@@ -1,12 +1,10 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using PlataformaDeEnsino.Application.AppServices.Interfaces;
 using PlataformaDeEnsino.Application.AppServices.Interfaces.InsitituicaoInterfaces;
 using PlataformaDeEnsino.Core.Entities;
 using PlataformaDeEnsino.Identity.Models;
